1e78ef84eba8017a577fa67a78fe39e70d9575fe
willard <willard_1946@qq.com>
2026-03-06 17:58:37 +0800

    logo

--- src/config/site.ts
+++ src/config/site.ts
@@ -11,12 +11,17 @@ export const NAVIGATION = [
   { name: 'Capabilities', href: '/capabilities' },
   { name: 'Use Cases', href: '/use-cases' },
   { name: 'Facilities', href: '/facilities' },
-  // { name: 'Request Quote', href: '/rfq' },
+  // { name: 'Request Quote', href: '/rfq' }, 
   { name: 'Documentation', href: '/documentation' },
 ] as const;
 
 export const SOCIAL_LINKS = {
   linkedin: 'https://linkedin.com/company/yourcompany',
+  youtube: 'https://youtube.com/yourcompany',
+  instagram: 'https://instagram.com/yourcompany',
+  xiaohongshu: 'https://www.xiaohongshu.com/user/profile/yourcompany',
+  wechat: 'https://weixin.qq.com/',
+  telegram: 'https://t.me/yourcompany',
   twitter: 'https://twitter.com/yourcompany',
   facebook: 'https://facebook.com/yourcompany',
 } as const;
