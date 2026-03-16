02d9b2577f09813a3a1eafc82028cca4708f3d4c
willard <willard_1946@qq.com>
2026-03-16 21:42:46 +0800

    首页图片生成

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { host: true },  // 或 host: '0.0.0.0'
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
